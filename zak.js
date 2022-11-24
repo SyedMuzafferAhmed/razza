65dedf851a971d0bbb7367d36a1bfc099188dbd4
SyedMuzafferAhmed <muzafsyed@gmail.com>
2022-11-24 09:24:47 +0530

    class of b22

---
+++ zak.js
@@ -0,0 +1,23 @@
+/*console.log(38&93)
+console.log(68/95)
+console.log(15^12)
+console.log(15-12)
+console.log(~5)
+console.log(2&1)
+console.log(3&1)
+console.log(4&1)
+console.log(5&1)*/
+
+/*console.log(~5)
+
+var a = 10, b = 5;
+a = a & b;
+console.log(a)
+a &= b;
+console.log(a)*/
+
+console.log(5 < 100 && 100 > 10 && 12 <10)
+console.log(5 > 100 || 100 > 10 || 12 <10)
+
+console.log(100 && 40 && 30)
+console.log()
